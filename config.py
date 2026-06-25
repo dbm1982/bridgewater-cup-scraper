05e66cc3f5d109576763633eb114ec77ab599e9e
dbm1982 <dbm1982@Hotmail.com>
2026-06-25 09:47:20 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 # BYSA Challenge Cup landing page where GotSport links live.
 # Update this only if BYSA changes their site URL.
-TOURNAMENT_LANDING_URL = "https://bridgewateryouthsoccer.org/challenge-cup"
+TOURNAMENT_LANDING_URL = "https://system.gotsport.com/org_event/events/52266"
+
